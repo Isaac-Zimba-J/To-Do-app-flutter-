4c4cec828d117e08a2622882a3224ac8d3f64d2b
Isaac-Zimba-J <zimbaisaacj2002@gmai.com>
2023-06-23 01:15:54 +0200

    Made a few changes and added a static model

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,49 +4,161 @@ import '../model/todoModel.dart';
 import '../constants/colors.dart';
 import '../widgets/todoItem.dart';
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final todosList = ToDo.todoList();
+  List<ToDo> _foundSearch = [];
+  final _todoController = TextEditingController();
+
+  @override
+  void initState() {
+    _foundSearch = todosList;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        child: Column(
-          children: [
-            SearchBox(),
-            Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: 50,
-                      bottom: 20,
-                    ),
-                    child: Text(
-                      'All todo\'s',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w500,
-                        color: tdWhite,
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(
+              children: [
+                SearchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(
+                          top: 50,
+                          bottom: 20,
+                        ),
+                        child: Text(
+                          'Tuma Sticky note\'s',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                            color: tdWhite,
+                          ),
+                        ),
                       ),
+                      for (ToDo todoo in _foundSearch.reversed)
+                        ToDoItem(
+                          todoObj: todoo,
+                          onToDoChanged: _handleToDoChange,
+                          onDeleteItem: _deleteToDoItem,
+                        ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                      controller: _todoController,
+                      decoration: InputDecoration(
+                          hintText: 'Add item iwe!', border: InputBorder.none),
                     ),
                   ),
-                  for (ToDo todoo in todosList)
-                    ToDoItem(
-                      todo: todoo,
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
+                  ),
+                  child: ElevatedButton(
+                    child: Text(
+                      '+',
+                      style: TextStyle(
+                        fontSize: 40,
+                      ),
                     ),
-                ],
-              ),
+                    onPressed: () {
+                      _addToDoItem(_todoController.text);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: tdBlue,
+                      minimumSize: Size(60, 60),
+                      elevation: 10,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          )
+        ],
       ),
     );
+  }
+
+//function to clcik todo
+  void _handleToDoChange(ToDo todo) {
+    setState(() {
+      todo.isDone = !todo.isDone;
+    });
+  }
+
+//function to delete todo item
+  void _deleteToDoItem(String Id) {
+    setState(() {
+      todosList.removeWhere((item) => item.Id == Id);
+    });
+  }
+
+//function to enter new todo
+  void _addToDoItem(String toDo) {
+    setState(() {
+      todosList.add(ToDo(
+          Id: DateTime.now().millisecondsSinceEpoch.toString(),
+          TodoText: toDo));
+    });
+    _todoController.clear();
+  }
+
+  //this fucntion only searches for the items mu todo
+  void _runFilter(String enteredKeyword) {
+    List<ToDo> results = [];
+    if (enteredKeyword.isEmpty) {
+      results = todosList;
+    } else {
+      results = todosList
+          .where((item) => item.TodoText!
+              .toLowerCase()
+              .contains(enteredKeyword.toLowerCase()))
+          .toList();
+    }
+    setState(() {
+      _foundSearch = results;
+    });
   }
 
   Widget SearchBox() {
@@ -54,6 +166,7 @@ class Home extends StatelessWidget {
       decoration: BoxDecoration(
           color: Colors.white, borderRadius: BorderRadius.circular(20)),
       child: TextField(
+        onChanged: (enteredWord) => _runFilter(enteredWord),
         decoration: InputDecoration(
             contentPadding: EdgeInsets.all(0),
             prefixIcon: Icon(
--- lib/widgets/todoItem.dart
+++ lib/widgets/todoItem.dart
@@ -4,30 +4,39 @@ import 'package:main_todo_app/constants/colors.dart';
 import '../model/todoModel.dart';
 
 class ToDoItem extends StatelessWidget {
-  final ToDo todo;
-  const ToDoItem({Key? key, required this.todo}) : super(key: key);
+  final ToDo todoObj;
+  final onToDoChanged;
+  final onDeleteItem;
+  const ToDoItem(
+      {Key? key,
+      required this.todoObj,
+      required this.onToDoChanged,
+      required this.onDeleteItem})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(bottom: 20),
       child: ListTile(
-        onTap: () {},
+        onTap: () {
+          onToDoChanged(todoObj);
+        },
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
         contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
         tileColor: Colors.white,
         leading: Icon(
-          todo.IsDone ? Icons.check_box : Icons.check_box_outline_blank,
+          todoObj.isDone ? Icons.check_box : Icons.check_box_outline_blank,
           color: tdBlue,
         ),
         title: Text(
-          todo.TodoText!,
+          todoObj.TodoText!,
           style: TextStyle(
             fontSize: 15,
             color: tdBGColor,
-            decoration: todo.IsDone ? TextDecoration.lineThrough : null,
+            decoration: todoObj.isDone ? TextDecoration.lineThrough : null,
           ),
         ),
         trailing: Container(
@@ -43,7 +52,9 @@ class ToDoItem extends StatelessWidget {
             color: Colors.white,
             iconSize: 18,
             icon: Icon(Icons.delete),
-            onPressed: () {},
+            onPressed: () {
+              onDeleteItem(todoObj.Id);
+            },
           ),
         ),
       ),
